2637c0b0fa48f1d90d6078eb5a24d7d2e983334d
SushiBanana <alysha.thean@gmail.com>
2023-06-20 02:06:38 +0800

    Insert slider dynamics and color picker

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("testing")
+const slider = document.querySelector(".slider")
+const sliderValue = document.querySelector(".slider-value")
+sliderValue.textContent = slider.value
+
+slider.oninput = function () {
+    sliderValue.textContent = `${this.value}`
+    sliderValue.append()
+}
