a33aa97ec77f4d1865111be6a113ac51fcb13602
SushiBanana <alysha.thean@gmail.com>
2023-06-21 00:24:30 +0800

    Create control panel

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const slider = document.querySelector(".slider")
 const sliderValue = document.querySelector(".slider-value")
-sliderValue.textContent = slider.value
+sliderValue.textContent = `${slider.value} x ${slider.value}`
 
 slider.oninput = function () {
-    sliderValue.textContent = `${this.value}`
+    sliderValue.textContent = `${this.value} x ${this.value}`
     sliderValue.append()
+}
+
+const paintbrush = document.querySelector(".paintbrush")
+const colorPicker = document.querySelector(".color-picker")
+
+colorPicker.oninput = function () {
+    paintbrush.setAttribute("style", 
+    `filter:drop-shadow(1px 1px 0 ${this.value})
+    drop-shadow(-1px 1px 0 ${this.value})
+    drop-shadow(1px -1px 0 ${this.value})
+    drop-shadow(-1px -1px 0 ${this.value}); 
+    -webkit-filter: drop-shadow(1px 1px 0 ${this.value})
+    drop-shadow(-1px 1px 0 ${this.value})
+    drop-shadow(1px -1px 0 ${this.value})
+    drop-shadow(-1px -1px 0 ${this.value});
+    `)
 }
